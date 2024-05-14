06c1f7c9b091cbbb14bfbf09e3644f86177533e4
AntonioDattolo <antonio19794.a@gmail.com>
2024-05-14 16:37:10 +0200

    M0

--- js/main.js
+++ js/main.js
@@ -5,13 +5,36 @@ const myConfig = {
 	data() {
 		
 		return {
-			message: 'Welcome to VueJS!'
+			toDo : [
+				{
+					title: 'fare la spesa',
+					result: false,
+				},
+				{
+					title: 'Aggiornare pc',
+					result: false,
+				},
+				{
+					title: 'Andare in palestra',
+					result: false,
+				},
+				{
+					title: 'Mettere la lavatrice',
+					result: false,
+				},
+				{
+					title: 'Dare da mangiare a Pixel',
+					result: false,
+				}
+			]
 		}
 	},
 	methods: {
-		stampaMsg() {
-			
+		do_it(index){
+			done = this.toDo[index].result
+			return done
 		}
+		
 	}
 };
 
